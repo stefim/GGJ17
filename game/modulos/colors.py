6bd6e1efc08185b65439e62c474569ab60988ad5
Jenaro <jenaropalombarini@gmail.com>
2017-01-21 10:59:12 -0300

    release v0.1

---
+++ game/modulos/colors.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# Colores en RGB
+# prefijo l significa light, d dark
+
+rojo = (255,0,0)
+lrojo = (255,155,155)
+drojo = (155,0,0)
+verde = (0,255,0)
+lverde = (155,255,155)
+dverde = (0,155,0)
+azul = (0,0,255)
+lazul = (155,155,255)
+dazul = (0,0,155)
+blanco = (255,255,255)
+negro = (0,0,0)
+gris = (155,155,155)
+amarillo = (255,255,0)
+lamarillo = (255,255,155)
+damarillo = (155,155,0)
+cyan = (0,255,255)
+lcyan = (155,255,255)
+dcyan = (0,155,155)
+violeta = (255,0,255)
+lvioleta = (255,155,255)
+dvioleta = (155,0,155)
+
+
--- game/run_game.py
+++ game/run_game.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
 
-import pygame, sys, os
-from modulos import *
+import pygame, sys, os, math
+from modulos.colors import *
 
 os.environ["SDL_VIDEO_CENTERED"] = "1"
 pygame.init()
 medidas_pantalla = (1024,700)
 
 ventana = pygame.display.set_mode(medidas_pantalla)
-pygame.display.set_caption("muevete")
+pygame.display.set_caption("game")
+reloj = pygame.time.Clock()
 
-menu = True
+cond_menu = True
+cond_jugar = True
+#flecha menu
+posX, posY = 200,450
+flecha_imagen = pygame.image.load("imagenes/sprite.png")
+seno = 0
+background_menu = pygame.image.load("imagenes/background_menu.png")
 
-while menu:
+# menu loop ----------------------------------------------------------
+while cond_menu:
+	#procesar eventos
 	for e in pygame.event.get():
 		if e.type == pygame.QUIT:
 			pygame.quit()
@@ -21,6 +30,37 @@ while menu:
 		if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
 			pygame.quit()
 			sys.exit()
-
+		if e.type == pygame.KEYDOWN and e.key == pygame.K_RETURN:
+			if posY == 450:
+				cond_menu = False
+			else:
+				pygame.quit()
+				sys.exit()
+		
+	key = pygame.key.get_pressed()
+	if key[pygame.K_UP]:
+		posY = 450
+	if key[pygame.K_DOWN]:
+		posY = 550
+			
+	#actualizar
+	reloj.tick(60)
+	seno += 0.1
+	#dibujar
+	ventana.blit(background_menu,(0,0))
+	ventana.blit(flecha_imagen,(posX,int(posY+math.degrees(math.sin(seno)) / 2 )) )
 	pygame.display.update()
 
+# juego loop -----------------------------------------------------------
+while cond_jugar:
+	#procesar eventos
+	for e in pygame.event.get():
+		if e.type == pygame.QUIT:
+			cond_jugar = False
+		if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+			cond_jugar = False
+	#actualizar
+	reloj.tick(60)
+	#dibujar
+	ventana.fill(rojo)
+	pygame.display.update()
